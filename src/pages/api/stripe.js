e0836334b7828790168a06c1f1ab4f83a547b907
aspandiyarKK <94121314+aspandiyarKK@users.noreply.github.com>
2024-06-03 04:47:13 +0500

    Update stripe.js

--- src/pages/api/stripe.js
+++ src/pages/api/stripe.js
@@ -10,10 +10,6 @@ export default async function handler(req, res) {
         mode: 'payment',
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
-        shipping_options: [
-          { shipping_rate: 'shr_1PNLFEERugAJemdwA64ZWYZz' },
-          { shipping_rate: 'shr_1PNLFEERugAJemdwA64ZWYZz' },
-        ],
         line_items: req.body.map(item => {
           const img = item.metadata.image.imgix_url
 
